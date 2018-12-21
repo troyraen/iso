256c4979294161e21842e19991b2c544f18cc9c2
troyraen <troy.raen@pitt.edu>
2018-12-21 12:36:09 -0500

    keep copy of input files in genisoinput.sh

--- scripts/genisoinput.sh
+++ scripts/genisoinput.sh
@@ -1,6 +1,6 @@
 ####
 #   This script generates the input file isocinput for Aaron's isochrone program
-#       based on history.data files in datadir
+#       based on history.data files in hdatadir
 #   Can run ./make_eep and ./make_iso at end
 #
 #   Assumes duplicate models (from backups and restarts) have
@@ -21,7 +21,9 @@ if [ $# -eq 0 ]
 fi
 
 cb=$1 # script input argument
-datadir="./data/tracks/c$cb" # history.data files should be here and named m#p#.data
+datadir="./data"
+hdatadir="${datadir}/tracks/c${cb}" # history.data files should be here and named m#p#.data
+eepinput="input.eep"
 isocinput="input.example" # will create this file
 isocoutput="isochrone_c$cb.dat" # make_iso writes to this file
 
@@ -29,7 +31,7 @@ declare -a hfiles # store names of history files for input.example
 # get the m#p## files
 mr=0
 for mp in 80 83 87 90 92 94 96 98; do
-    hdat=${datadir}/m${mr}p${mp}.data
+    hdat=${hdatadir}/m${mr}p${mp}.data
     if [ -e $hdat ]; then
         hfiles=("${hfiles[@]}" "m${mr}p${mp}.data")
     fi
@@ -37,7 +39,7 @@ done
 # get the other files
 for mr in {1..5}; do # for loops ensure list is ordered in increasing mass
     for mp in {0..9}; do
-        hdat=${datadir}/m${mr}p${mp}.data
+        hdat=${hdatadir}/m${mr}p${mp}.data
         if [ -e $hdat ]; then
             hfiles=("${hfiles[@]}" "m${mr}p${mp}.data")
         fi
@@ -56,7 +58,7 @@ example
 #initial Y, initial Z, [Fe/H], [alpha/Fe], v/vcrit (space separated)
    0.2703 1.4e-02   0.00        0.00     0.0
 #data directories: 1) history files, 2) eeps, 3) isochrones
-${datadir}
+${hdatadir}
 data/eeps
 data/isochrones
 #read history_columns
@@ -71,6 +73,9 @@ log10
 51
 5.0
 10.11" >> $isocinput
+
+# keep a copy of input files
+cp ${isocinput} ${eepinput} ${datadir}/.
 
 # uncomment these lines to run make_eep and make_iso
 export ISO_DIR=$(pwd)
